8dfce849e6dadc08e8a24a19daa3dab40d8735a8
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2026-04-28 16:39:53 +0100

    chore: add nix image configuration
    Signed-off-by: João Sereno <joaohsereno@tecnico.ulisboa.pt>

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "Reproducible and Immutable NixOS Images";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    evident-instance = {
+      url = "gitlab:dpss-inesc-id/achilles-cvm/dev?dir=instance";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    evident-client = {
+      url = "gitlab:dpss-inesc-id/achilles-cvm/dev?dir=client";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "riscv64-linux"
+      ];
+      perSystem = { config, pkgs, ... }: {
+        packages =
+        let
+          mkBundle = inputs.evident-instance.apps.x86_64-linux.mkBundle;
+        in
+        {
+          root-ca = pkgs.callPackage ./src {
+            platform = "gce";
+            inherit inputs;
+            evidentInstancePackage = mkBundle {
+              mandatoryFeature = "snp_gce";
+              optionalFeatures = [
+                "debug"
+              ];
+            };
+            withDebug = true;
+          };
+          intermediate-ca-ec2 = pkgs.callPackage ./src {
+            platform = "ec2";
+            inherit inputs;
+            evidentInstancePackage = mkBundle {
+              mandatoryFeature = "snp_ec2";
+              optionalFeatures = [
+                "debug"
+                "request_certificate"
+              ];
+              certificateIssuerEndpoint = "root-ca.evident.joaohs.com";
+            };
+            withDebug = true;
+          };
+          intermediate-ca-gce = pkgs.callPackage ./src {
+            platform = "gce";
+            inherit inputs;
+            evidentInstancePackage = mkBundle {
+              mandatoryFeature = "snp_gce";
+              optionalFeatures = [
+                "debug"
+                "request_certificate"
+              ];
+              certificateIssuerEndpoint = "root-ca.evident.joaohs.com";
+            };
+            withDebug = true;
+          };
+        };
+      };
+    };
+}
---
+++ src/confidential-ca/confidential-ca.nix
@@ -0,0 +1,50 @@
+{
+  pkgs,
+  evidentClientPackage,
+  ...
+}:
+let
+  username="ca";
+  port="5010";
+  grpcCert="/etc/evident/pki/grpc/public/grpc.crt.pem";
+  grpcKey="/etc/evident/pki/grpc/private/grpc.key.pem";
+  selfSignedCert="/etc/evident/pki/instance/public/instance-root.crt.pem";
+  chainedCert="/etc/evident/pki/instance/public/instance.crt.pem";
+  instanceKey="/etc/evident/pki/instance/private/instance.key.der";
+  startScript = pkgs.writeShellScript "confidential-ca-start" ''
+    if [ -f ${chainedCert} ]; then
+      cert=${chainedCert}
+    else
+      cert=${selfSignedCert}
+    fi
+
+    exec ${evidentClientPackage}/bin/evident ${port} ${"$"}cert ${instanceKey} ${grpcCert} ${grpcKey}
+  '';
+in
+{
+  users.users.${username} = {
+    createHome = false;
+    isSystemUser = true;
+    group = "${username}";
+    shell = "${pkgs.shadow}/bin/nologin";
+  };
+  users.groups.worker = {};
+
+  systemd.services.confidential-ca = {
+    description = "Confidential Certificate Authority";
+    after = [ "evident-server.service" ];
+    requires = [ "evident-server.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${startScript}";
+      WorkingDirectory = "/var/lib/${username}";
+      User = "root";
+      Restart = "on-failure";
+      RestartSec = "5s";
+      TimeoutStartSec = "0";
+    };
+  };
+
+}
